bbf04d9692c15e7355d27e6f3a4bfe0cf68c259a
sam_ <45014739+sbordel@users.noreply.github.com>
2021-02-04 17:54:35 -0500

    E2: created new js file & added mushrooms images
    -created arrays containing toxic/edible mushrooms & an array combining the two.
    -also added new fonts

--- exercises/slamina-new-game-plus/js/script.js
+++ exercises/slamina-new-game-plus/js/script.js
@@ -1,215 +1,66 @@
-"use strict";
 
-const animals = [
-    "aardvark",
-    "alligator",
-    "alpaca",
-    "antelope",
-    "ape",
-    "armadillo",
-    "baboon",
-    "badger",
-    "bat",
-    "bear",
-    "beaver",
-    "bison",
-    "boar",
-    "buffalo",
-    "bull",
-    "camel",
-    "canary",
-    "capybara",
-    "cat",
-    "chameleon",
-    "cheetah",
-    "chimpanzee",
-    "chinchilla",
-    "chipmunk",
-    "cougar",
-    "cow",
-    "coyote",
-    "crocodile",
-    "crow",
-    "deer",
-    "dingo",
-    "dog",
-    "donkey",
-    "dromedary",
-    "elephant",
-    "elk",
-    "ewe",
-    "ferret",
-    "finch",
-    "fish",
-    "fox",
-    "frog",
-    "gazelle",
-    "gila monster",
-    "giraffe",
-    "gnu",
-    "goat",
-    "gopher",
-    "gorilla",
-    "grizzly bear",
-    "ground hog",
-    "guinea pig",
-    "hamster",
-    "hedgehog",
-    "hippopotamus",
-    "hog",
-    "horse",
-    "hyena",
-    "ibex",
-    "iguana",
-    "impala",
-    "jackal",
-    "jaguar",
-    "kangaroo",
-    "koala",
-    "lamb",
-    "lemur",
-    "leopard",
-    "lion",
-    "lizard",
-    "llama",
-    "lynx",
-    "mandrill",
-    "marmoset",
-    "mink",
-    "mole",
-    "mongoose",
-    "monkey",
-    "moose",
-    "mountain goat",
-    "mouse",
-    "mule",
-    "muskrat",
-    "mustang",
-    "mynah bird",
-    "newt",
-    "ocelot",
-    "opossum",
-    "orangutan",
-    "oryx",
-    "otter",
-    "ox",
-    "panda",
-    "panther",
-    "parakeet",
-    "parrot",
-    "pig",
-    "platypus",
-    "polar bear",
-    "porcupine",
-    "porpoise",
-    "prairie dog",
-    "puma",
-    "rabbit",
-    "raccoon",
-    "ram",
-    "rat",
-    "reindeer",
-    "reptile",
-    "rhinoceros",
-    "salamander",
-    "seal",
-    "sheep",
-    "shrew",
-    "silver fox",
-    "skunk",
-    "sloth",
-    "snake",
-    "squirrel",
-    "tapir",
-    "tiger",
-    "toad",
-    "turtle",
-    "walrus",
-    "warthog",
-    "weasel",
-    "whale",
-    "wildcat",
-    "wolf",
-    "wolverine",
-    "wombat",
-    "woodchuck",
-    "yak",
-    "zebra"
-  ];
+let toxicMush = [];
+let numtoxicMush = 29;
 
-  const QUESTION_DELAY = 2000; 
+let edibleMush = [];
+let numedibleMush = 31;
 
-  let currentAnswer = `Click to begin.`;
-  let currentAnimal = ``;
+let mushrooms = [];
+
+//let currentMushroom;
+
+function preload() {
+
+    for (let i = 0; i < numtoxicMush; i++) {
+        toxicMush[i] = loadImage("assets/images/toxic/toxicmush_" + i + ".png");
+    }
+
+    for (let i = 0; i < numedibleMush; i++) {
+        edibleMush[i] = loadImage("assets/images/edible/ediblemush_" + i + ".png");
+    }
+
+    mushrooms = [].concat(toxicMush, edibleMush);
+
+    console.log(mushrooms);
+}
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
 
-    if (annyang) {
-        let commands = {
-          'I think it is *animal': guessAnimal
-        };
-        annyang.addCommands(commands);
-        annyang.start();
-
-        textSize(100);
-        textStyle(BOLD);
-        textAlign(CENTER);
-    }
+    noLoop();
 }
-
 
 function draw() {
-    background(0);
+    background(194, 201, 252);
 
-    
-    displayAnswer();
+    imageMode(CENTER);
+    let randomMush = random(toxicMush);
+
+    //imageSize(width / 2, height / 2);
+
+    image(randomMush, width / 2, height / 2);
 }
 
-function displayAnswer() {
-    if (currentAnswer === currentAnimal) {
-        fill(0, 255, 0);
+/*function checkIfToxic(){
+
+    if( toxicMush.includes( currentMushroom ) ){
+      // do something if array does include the mushroom
     }
-    else{
-        fill(255, 0, 0);
+    else {
+      // do something if array does NOT include the mushroom
     }
-    text(currentAnswer, width / 2, height / 2);
-}
-
-function sayAnimalBackwards(animal) {
- let reverseAnimal = reverseString(animal);
- responsiveVoice.speak(reverseAnimal);
-}
-
-
-//Reverses the provided string
-
-function reverseString(string) {
-    // Split the string into an array of characters
-    let characters = string.split('');
-    // Reverse the array of characters
-    let reverseCharacters = characters.reverse();
-    // Join the array of characters back into a string
-    let result = reverseCharacters.join('');
-    // Return the result
-    return result;
-  } 
-
-
-function guessAnimal(animal) {
-    // Convert the guess to lowercase to match the answer format
-    currentAnswer = animal.toLowerCase();
   }
   
-  //Reset the answer text, get a new random animal, say its name
-
-  function nextQuestion() {
-    currentAnswer = ``;
-    currentAnimal = random(animals);
-    sayAnimalBackwards(currentAnimal);
-  }
+  function checkIfEdible(){
   
-  //When the user clicks, go to the next question
-  function mousePressed() {
-    nextQuestion();
+    if( edibleMush.includes( currentMushroom ) ){
+      // do something if array does include the mushroom
+    }
+    else {
+      // do something if array does NOT include the mushroom
+    }
+  }
+*/ 
+function mousePressed() {
+
   }
