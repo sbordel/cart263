a0effeee4f5b3e89787755e4a968d193a2ca62cd
sam_ <45014739+sbordel@users.noreply.github.com>
2021-02-04 23:55:35 -0500

    E2: trying to make voice callbacks when answer is tried
    failed : (

--- exercises/slamina-new-game-plus/js/script.js
+++ exercises/slamina-new-game-plus/js/script.js
@@ -1,66 +1,71 @@
+"use strict";
 
 let toxicMush = [];
-let numtoxicMush = 29;
-
 let edibleMush = [];
-let numedibleMush = 31;
+
+let toxicButton;
+let edibleButton;
 
 let mushrooms = [];
 
-//let currentMushroom;
+let randomMush;
 
 function preload() {
 
-    for (let i = 0; i < numtoxicMush; i++) {
-        toxicMush[i] = loadImage("assets/images/toxic/toxicmush_" + i + ".png");
-    }
+  for (let i = 0; i < 29; i++) {
+      toxicMush[i] = loadImage("assets/images/toxic/toxicmush_" + i + ".png");
+  }
 
-    for (let i = 0; i < numedibleMush; i++) {
-        edibleMush[i] = loadImage("assets/images/edible/ediblemush_" + i + ".png");
-    }
+  for (let i = 0; i < 31; i++) {
+      edibleMush[i] = loadImage("assets/images/edible/ediblemush_" + i + ".png");
+  }
 
-    mushrooms = [].concat(toxicMush, edibleMush);
+  mushrooms = toxicMush.concat(edibleMush);
 
-    console.log(mushrooms);
+   /* textSize(70);
+    textStyle(BOLD);
+    textAlign(CENTER);
+    textFont("fontTwo");*/  
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
 
-    noLoop();
+  toxicButton = createButton('toxic');
+  toxicButton.position(width * 0.8, height / 2, 65);
+  toxicButton.mousePressed(toxicGuess);
+
+  edibleButton = createButton('edible');
+  edibleButton.position(width * 0.18, height / 2, 65);
+  edibleButton.mousePressed(toxicGuess);
+
+  textSize(64);
+  textStyle(BOLD);
+  textAlign(CENTER);
 }
 
 function draw() {
-    background(194, 201, 252);
+  background(194, 201, 252);
 
-    imageMode(CENTER);
-    let randomMush = random(toxicMush);
+  imageMode(CENTER);
+  let randomMush = random(mushrooms);
 
-    //imageSize(width / 2, height / 2);
+  image(randomMush, width / 2, height / 2);
 
-    image(randomMush, width / 2, height / 2);
+  noLoop();
+
 }
 
-/*function checkIfToxic(){
+function toxicGuess() {
+  if (toxicMush.includes(image < 29)){
+    responsiveVoice.speak("this mushroom is toxic", "UK English Female", {onend: newMushroom}, {pitch: 6});
+  }
+  else {
+    responsiveVoice.speak("this mushroom is edible", "UK English Female", {onend: newMushroom}, {pitch: 0.5});
+  }
+}
 
-    if( toxicMush.includes( currentMushroom ) ){
-      // do something if array does include the mushroom
-    }
-    else {
-      // do something if array does NOT include the mushroom
-    }
-  }
-  
-  function checkIfEdible(){
-  
-    if( edibleMush.includes( currentMushroom ) ){
-      // do something if array does include the mushroom
-    }
-    else {
-      // do something if array does NOT include the mushroom
-    }
-  }
-*/ 
-function mousePressed() {
+function newMushroom(){
+ draw(randomMush);
+}
 
-  }
