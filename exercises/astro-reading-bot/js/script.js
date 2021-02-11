e112b55889d369f17a596b207d3473edb2ee8fbf
sam_ <45014739+sbordel@users.noreply.github.com>
2021-02-11 15:19:35 -0500

    E3: created form in window to gather info

--- exercises/astro-reading-bot/js/script.js
+++ exercises/astro-reading-bot/js/script.js
@@ -1,67 +1,76 @@
 "use strict";
 
-let spyProfile = {
-    name: `?`,
-    alias: `?`,
-    secretWeapon: `?`,
-    password: `?`,
+let dayOfBirth; 
+let monthOfBirth; 
+let birthday = monthOfBirth + ` ` + dayOfBirth ;
+
+let username;
+
+
+let zodiacProfile = {
+    username: `?`,
+    birthday: `?`,
+    zodiacSign: `?`,
+    animal: `?`,
 };
 
-let instrumentData = undefined;
-let objectData = undefined;
-let tarotData = undefined;
+let zodiacData = undefined;
 
 function preload() {
-    zodiacData = loadJSON(`json/zodiac-animals.json`);
+    zodiacData = loadJSON(`js/json/zodiac-animals.json`);
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
 
-    let data = JSON.parse(localStorage.getItem(`spy-profile-data`));
+    /*
+    let data = JSON.parse(localStorage.getItem(`zodiac-profile-data`));
     if (data !== null) {
-       let password = prompt(`what is your password?`);
-       if (password === data.password){
+       let username = prompt(`what is your username?`);
+       if (username === data.username){
         }
-        spyProfile.name = data.name;
-        spyProfile.alias = data.alias;
-        spyProfile.secretWeapon = data.secretWeapon;
-        spyProfile.password = data.password;
+        zodiacProfile.username = data.username;
+        zodiacProfile.birthday = data.birthday;
+        zodiacProfile.zodiacSign = data.zodiacSign;
+        zodiacProfile.animal = data.animal;
     }
     else {
-        generateSpyProfile();
-    }
+        generateZodiacProfile();
+    }*/
 }
 
-function generateSpyProfile() {
-    spyProfile.name = prompt(`what is your name.`);
+function getMonth() {
+    document.getElementById("inputMonth").innerHTML = "Hello World";
+
+  usernname = document.getElementById("username").value;
+  dayOfBirth =  document.getElementById("inputDay").value;
+  monthOfBirth = document.getElementById("inputMonth").value;
+  submitOK = "true";
+  }
+
+/*
+function generateZodiacProfile() {
+    zodiacProfile.username = prompt(`what is your username?`);
     
-    let instrument = random(instrumentData.instruments);
-    spyProfile.alias = `The ${instrument}`;
-    spyProfile.secretWeapon = random(objectData.objects);
 
-    let card = random(tarotData.tarot_interpretations);
-    spyProfile.password = random(card.keywords);
 
-    localStorage.setItem(`spy-profile-data`, JSON.stringify(spyProfile));
-}
+    localStorage.setItem(`zodiac-profile-data`, JSON.stringify(zodiacProfile));
+}*/
 
 function draw() {
     background(0);
+    /*let profile = ` ; - )
 
-    let profile = `** SPY PROFILE! DO NOT DISTRIBUTE! **
-
-    Name: ${spyProfile.name}
-    Alias: ${spyProfile.alias}
-    Secret Weapon: ${spyProfile.secretWeapon}
-    Password: ${spyProfile.password}`;
-
+    username: ${username.name}
+    birthday: ${birthday.name}
+    zodiac sign: ${zodiacSign.name}
+    animal: ${animal.name}`;
     push();
     textFont(`Courier, monospace`);
     textSize(24);
     textAlign(LEFT, TOP);
     fill(255);
-    text(profile, 100, 100);
+    text(zodiacProfile, 100, 100);
 
-    pop();
+    pop();*/
 }
