a7ccad0d23cbc8606973b7518b5650096c9c3be2
sam_ <45014739+sbordel@users.noreply.github.com>
2021-03-08 00:17:55 -0500

    P1: changing shirt position pt3

--- projects/a-night-at-the-movies/js/script.js
+++ projects/a-night-at-the-movies/js/script.js
@@ -13,11 +13,11 @@ sam bordeleau
 // variables
 let balloonSunny;
 let balloonSunnyX = -160;
-let balloonSunnyY = -25;
+let balloonSunnyY = -15;
 
 let balloonJP;
 let balloonJPX = 105;
-let balloonJPY = -40;
+let balloonJPY = -30;
 
 let shirtWhite;
 let shirtPlaid;
