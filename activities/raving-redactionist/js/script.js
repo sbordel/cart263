ef38a8d8611e9a9c229b03bd5e3611aa380a7b69
sam_ <45014739+sbordel@users.noreply.github.com>
2021-03-16 14:11:01 -0400

    A7: edited html formatting and added click events to censor some parts of the text

--- activities/raving-redactionist/js/script.js
+++ activities/raving-redactionist/js/script.js
@@ -1,11 +1,25 @@
 /**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Raving Redactionist CSS
 */
 
 "use strict";
 
-// Code goes here
+$('.top-secret').on('click', redact);
+setInterval(revelation, 500);
+
+function redact(event) {
+    $(this).removeClass('revealed');
+    $(this).addClass('redacted');
+}
+
+function revelation() {
+    $('.redacted').each(attemptReveal);
+}
+
+function attemptReveal() {
+    let r = Math.random();
+    if (r < 0.1) {
+    $(this).removeClass('redacted');
+    $(this).addClass('revealed');
+  }
+}
