7eac6cadd75ec4c6afe20be74db8442de0730690
sam_ <45014739+sbordel@users.noreply.github.com>
2021-03-17 23:43:54 -0400

    E7: copied activity folder - start code

---
+++ exercises/friends-only/js/script.js
@@ -0,0 +1,25 @@
+/**
+Raving Redactionist CSS
+*/
+
+"use strict";
+
+$('.top-secret').on('click', redact);
+setInterval(revelation, 500);
+
+function redact(event) {
+    $(this).removeClass('revealed');
+    $(this).addClass('redacted');
+}
+
+function revelation() {
+    $('.redacted').each(attemptReveal);
+}
+
+function attemptReveal() {
+    let r = Math.random();
+    if (r < 0.1) {
+    $(this).removeClass('redacted');
+    $(this).addClass('revealed');
+  }
+}
