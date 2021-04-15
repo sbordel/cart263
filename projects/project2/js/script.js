d7a7c6e25dafe4b421e053f21d5544f0a67da4d1
sam_ <45014739+sbordel@users.noreply.github.com>
2021-04-15 00:57:59 -0400

    P2: added variables for droppedFile date properties + added/changed data in json file

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -1,26 +1,38 @@
-/**
-SCREENS LIKE GROW LAMPS
+/* ****** ****** 
+
+WELCOME TO THE ELECTRIC GARDEN
 Samuelle Bordeleau
 
-This is a prototype for the index page of my project
-*/
+******  ****** */
 
 "use strict";
 
+//variable assigned to a file that is dropped in the drop zone
 let droppedFile;
-//let splicedDate;
-let newDate;
 
-let fileSize;
-let fileType;
-let fileDate;
+//variables for droppedFile; properties
+let fileSize; // number
+let fileType; // string
+let fileDate; // date
+
+//variables for droppedFile; date properties 
+// all numbers
+let fileYear; 
+let fileMonth; 
+let filedateNum; 
+let fileHours; 
+let fileMin; 
+let fileSec; 
+let fileMilli; 
+let fileDay; 
+
 
 function setup() {
-  // create canvas
+  // Creates a canvas
   var dropZone = createCanvas(windowWidth, windowHeight);
   dropZone.parent('p5Canvas');
   background(0);
-  // Add an event for when a file is dropped onto the canvas
+  // Adds an event for when a file is dropped onto the canvas
   dropZone.drop(gotFile);
 }
 
@@ -37,8 +49,6 @@ function gotFile(file) {
   console.log(file);
 
   droppedFile = file;
-
-  //droppedFile = createP(file.data);
   // If it's an image file
   if (file.type === 'image') {
     // Create an image DOM element but don't show it
@@ -49,30 +59,51 @@ function gotFile(file) {
   } else {
     successImg.style.display = 'block';
   }
-
+  // assign droppedFile properties to variables
   fileSize = file.size;
   fileType = file.subtype;
   fileDate = file.file.lastModifiedDate;
 
-  //splicedDate();
+  splicedDate();
 }
 
-/** need to fix this because fileDate isnt a String **/
-//reference here (go to RegExp section): 
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
-//reg exp character classes:
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Character_Classes
+/* splices the droppedFile's timestamp into corresponding time property variables
+   timestamp format ==> Wed Apr 14 2021 11:41:13 GMT-0400 (GMT-04:00) */
+
+function splicedDate() {
+  fileYear = fileDate.getFullYear(); // YYYY
+  fileMonth = fileDate.getMonth(); // 0-11
+  filedateNum = fileDate.getDate(); // 1-31
+  fileHours = fileDate.getHours(); // 0-23
+  fileMin = fileDate.getMinutes(); // 0-59
+  fileSec = fileDate.getSeconds(); // 0-59
+  fileMilli = fileDate.getMilliseconds(); // 0-999
+  fileDay = fileDate.getDay(); // 0-6
+
+  console.log(fileYear);
+  console.log(fileMonth);
+  console.log(filedateNum);
+  console.log(fileHours);
+  console.log(fileMin);    
+  console.log(fileSec);
+  console.log(fileMilli);
+  console.log(fileDay);
+}
 
 /*
-function splicedDate() {
-  let splitString = split(fileDate, ' ');
-  let day = text(splitString[0], 5, 30);
-  let month = text(splitString[1], 5, 50);
-  let date = text(splitString[2], 5, 70);
+JSON FILE DATA TYPE
 
-  console.log(fileDate);
-  console.log(day);
-  console.log(month);
-  console.log(date);
-}
+stem, button: boolean
+countrycode: string
+leaf, petals, day, hour, minutes, seconds, millis, key: number
 */
+
+
+/*
+NOTES FOR PETAL VARIABLES IN JSON FILE
+
+tulip = -1
+not defined = 0
+chaotic = 99
+*/
+
