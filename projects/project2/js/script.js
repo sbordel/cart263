adf304579fb336e53726b0ea2b9f4ba395090496
sam_ <45014739+sbordel@users.noreply.github.com>
2021-04-19 19:45:25 -0400

    P2: edited and added comments to script file

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -1,21 +1,21 @@
-/* ****** ****** 
+/************************** 
 
 WELCOME TO THE ELECTRIC GARDEN
 Samuelle Bordeleau
 
-******  ****** */
+**************************/
 
 "use strict";
 
-//variable assigned to a file that is dropped in the drop zone
+/* variable assigned to a file that is dropped in the drop zone */
 let droppedFile;
 
-//variables for droppedFile properties
+/* variables for droppedFile properties */
 let fileSize; // number
 let fileType; // string
 let fileDate; // date
 
-//variables for droppedFile date properties 
+/* variables for droppedFile date properties */
 let fileYear;
 let fileMonth;
 let filedateNum;
@@ -25,16 +25,15 @@ let fileSec;
 let fileMilli;
 let fileDay;
 
-//variables for user's location
+/* variables for user's location */
 let userCountry;
 let userRegion;
 
-//variables each flower's properties (flowerData)
+/* variables each flower's properties (flowerData) */
 let flowerData;
 let flowerMonth;
 let flowerDay;
 let flowerSeason;
-
 
 function setup() {
   // Creates a canvas
@@ -42,24 +41,20 @@ function setup() {
   dropZone.parent('p5Canvas');
   background(0);
 
-  flowerData = loadJSON('assets/data/flower_test.json');
-  //console.log(flowerData.key)
-
   // Adds an event for when a file is dropped onto the canvas
   dropZone.drop(gotFile);
 }
-
 
 function draw() {
   noLoop();
 }
 
-// canvas resizes when window is resized
+/* canvas resizes when window is resized */
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-// gets user location with ipify API 
+/* gets user location with ipify API */
 $(function getUserLocation() {
   $.getJSON("https://geo.ipify.org/api/v1?apiKey=at_OZGboRjmnwnVC8rVin6gnzz0BOWWQ",
     function (text) {
@@ -97,7 +92,7 @@ function gotFile(file) {
   mapflowerSeasons();
 };
 
-// splices the droppedFile's timestamp into corresponding time property variables
+/* splices the droppedFile's timestamp into corresponding time property variables */
 function splicedDate() {
   fileYear = fileDate.getFullYear(); // YYYY
   fileMonth = fileDate.getMonth(); // 0-11
@@ -108,43 +103,47 @@ function splicedDate() {
   fileMilli = fileDate.getMilliseconds(); // 0-999
 };
 
-// assigns flower date properties to variables: 
+/* assigns flowerData date/time properties to variables */
 function mapflowerSeasons() {
-  flowerDay = flowerData.flowers[11].day; 
- // remaps the "hour" property of a flower to a value between the range of 0-11 (corresponding to the 12 months)
-  let monthDecimal = map(flowerData.flowers[11].hour, 0, 23, 0, 11); 
-  flowerMonth = parseInt(monthDecimal); //convert result to integer
+  //load json file with flower data
+  flowerData = loadJSON('assets/data/flowers.json');
+  //assigns day property to flowerDay
+  flowerDay = flowerData.flowers[11].day;
+  // remaps the "hour" property of a flower to a value between the range of 0-11 (corresponding to the 12 months)
+  // assigns numeral value monthDecimal
+  let monthDecimal = map(flowerData.flowers[11].hour, 0, 23, 0, 11);
+  flowerMonth = parseInt(monthDecimal); //convert result to integer and assigns it to flowerMonth
   flowerSeasons();
 };
 
 function flowerSeasons() {
-  // spring : march 20 to june 21
+  // spring == march 20 to june 21
   if ((flowerMonth == 3 && flowerDay >= 20 && flowerDay <= 31)
     || (flowerMonth == 4 && flowerDay >= 1 && flowerDay <= 30)
     || (flowerMonth == 5 && flowerDay >= 1 && flowerDay <= 31)
     || (flowerMonth == 6 && flowerDay >= 1 && flowerDay <= 21)) {
     flowerSeason = "Spring";
   }
-  // summer : june 21 to september 22
+  // summer == june 21 to september 22
   else if ((flowerMonth == 6 && flowerDay >= 21 && flowerDay <= 31)
     || (flowerMonth == 7 && flowerDay >= 1 && flowerDay <= 31)
     || (flowerMonth == 8 && flowerDay >= 1 && flowerDay <= 31)
     || (flowerMonth == 9 && flowerDay >= 1 && flowerDay <= 22)) {
     flowerSeason = "Summer";
   }
-  // fall : september 22 to december 21
+  // fall == september 22 to december 21
   else if ((flowerMonth == 9 && flowerDay >= 22 && flowerDay <= 31)
     || (flowerMonth == 10 && flowerDay >= 1 && flowerDay <= 31)
     || (flowerMonth == 11 && flowerDay >= 1 && flowerDay <= 31)
     || (flowerMonth == 12 && flowerDay >= 1 && flowerDay <= 21)) {
     flowerSeason = "Fall";
   }
-   // winter : december 21 to march 20
-   else if ((flowerMonth == 11 && flowerDay >= 21 && flowerDay <= 31)
-   || (flowerMonth == 0 && flowerDay >= 1 && flowerDay <= 31)
-   || (flowerMonth == 1 && flowerDay >= 1 && flowerDay <= 31)
-   || (flowerMonth == 3 && flowerDay >= 1 && flowerDay <= 20)) {
-   flowerSeason = "Winter";
- }
+  // winter == december 21 to march 20
+  else if ((flowerMonth == 11 && flowerDay >= 21 && flowerDay <= 31)
+    || (flowerMonth == 0 && flowerDay >= 1 && flowerDay <= 31)
+    || (flowerMonth == 1 && flowerDay >= 1 && flowerDay <= 31)
+    || (flowerMonth == 3 && flowerDay >= 1 && flowerDay <= 20)) {
+    flowerSeason = "Winter";
+  }
   console.log(flowerSeason);
 };
